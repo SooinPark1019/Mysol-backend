d7f382734df6d03fd9e65a202969c97eb341d19d
SooinPark1019 <hyperion1019@DESKTOP-0K9R2DU>
2025-02-21 21:46:24 +0900

    add API prefix

--- mysol/api.py
+++ mysol/api.py
@@ -4,4 +4,4 @@ from mysol.app.User.views import user_router
 
 api_router = APIRouter()
 
-api_router.include_router(user_router)
+api_router.include_router(user_router, prefix="/users", tags=["users"])
